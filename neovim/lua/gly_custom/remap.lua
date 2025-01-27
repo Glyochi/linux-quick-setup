e944e7d75ee5928d96bc53d340d6d2661ed7627d
gly <duongvh1806@gmail.com>
2025-01-26 22:16:45 -0600

    added remap

--- neovim/lua/gly_custom/remap.lua
+++ neovim/lua/gly_custom/remap.lua
@@ -1,4 +1,21 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move the whole highlighted block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- paste without replacing the buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+
+
+-- supposedly copy to clipboard (+ register) and can use outside of vim
+-- vim.keymap.set("n", "<leader>y", "\"+y")
+-- vim.keymap.set("v", "<leader>y", "\"+y")
+-- vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+
+
 return {
 }
 
