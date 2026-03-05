d76579c31a3cf48bb426ff0eb62b0389da9c6e02
Gly <duongvh1806@gmail.com>
2026-03-04 23:28:19 -0600

    new colorscheme

--- neovim/lua/gly_custom/plugins/themes.lua
+++ neovim/lua/gly_custom/plugins/themes.lua
@@ -1,15 +1,3 @@
-function TransparentBackground()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	callback = function()
-		TransparentBackground()
-	end,
-})
-
 return {
 	{
 		"ellisonleao/gruvbox.nvim",
@@ -25,9 +13,31 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
+	},
+	{
+		"everviolet/nvim",
+		name = "evergarden",
+		lazy = false,
+		priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
 		config = function()
-			vim.cmd("colorscheme tokyonight")
-			TransparentBackground()
+			vim.cmd("colorscheme evergarden-winter")
 		end,
+		opts = {
+			theme = {
+				variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+				accent = "green",
+			},
+			editor = {
+				transparent_background = false,
+				sign = { color = "none" },
+				float = {
+					color = "mantle",
+					solid_border = false,
+				},
+				completion = {
+					color = "surface0",
+				},
+			},
+		},
 	},
 }
